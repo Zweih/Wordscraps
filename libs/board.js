b190f9a8aa5b00cf6ae489092f27fd9081d43834
Zweih <fernandon299@gmail.com>
2018-09-28 17:28:03 -0400

    Add joining words.

--- libs/board.js
+++ libs/board.js
@@ -3,40 +3,140 @@ const wordPools = require("./valid-dictionary/word-pools");
 class Board {
   constructor() {
     this.grid = new Array(10);
+    this.currentWords = [];
+    this.posArr = [];
+    this.isNextVert = false;
+
     this.letterPool = this.randLetterPool(wordPools);
     this.wordPool = wordPools[this.letterPool];
-
-    this.letterPool = this.shufflePool(this.letterPool.split()).toString();
-    this.wordPool = this.shufflePool(this.wordPool);
+    this.letterPool = this.shuffleArr(this.letterPool.split()).toString();
+    this.wordPool = this.shuffleArr(this.wordPool);
 
     for(let i = 0; i < this.grid.length; i++) {
       this.grid[i] = new Array(10);
+      
+      for(let j = 0; j < this.grid.length; j++) {
+        this.grid[i][j] = [null, false];
+      }
     }
 
     console.log(this.letterPool);
     console.log(this.wordPool);
 
-    this.grid = this.across(this.wordPool[0], [5,5], 0)
-    console.log(this.grid);
+    this.firstWord();
+    this.continue();
+  }
+
+  firstWord() {
+    let valid = false;
+    
+    for(let i = 0; i < this.wordPool.length && !valid; i++) {
+      const valid = this.horz(this.wordPool[i], [5,5], 0);
+    }
   }
 
   // 0, 1 for right or left
-  across(word, startPos, dir) {
+  horz(word, startPos, dir) {
     const dupGrid = JSON.parse(JSON.stringify(this.grid))
+    const spots = [];
     let valid = this.isInBounds(word, startPos, 2 + dir);
 
     for(let i = 0; i < word.length && valid; i++) {
-      if(!dupGrid[startPos[0]][startPos[1] + i]) {
-        dupGrid[startPos[0]][startPos[1] + i] = word[i];
+      if(!dupGrid[startPos[0]][startPos[1] + i][0] || dupGrid[startPos[0]][startPos[1] + i][0] === word[i]) {
+        dupGrid[startPos[0]][startPos[1] + i][0] = word[i];
+        spots.push([startPos[0] ,startPos[1] + i]);
       } else {
-        return this.grid;
+        valid = false;
       }
     }
 
-    return dupGrid;
+    if(!valid) {
+      return false;
+    }
+
+    this.isNextVert = true;
+
+    this.grid = dupGrid;
+    this.posArr = this.posArr.concat(spots);
+    this.wordPool.splice(this.wordPool.indexOf(word), 1);
+    this.currentWords.push(word);
+
+    return true;
   }
 
-  // 0,1,2,3 for up, down, right, or left (respectively).
+  // 0, 1 for down or up
+  vert(word, startPos, dir) {
+    const dupGrid = JSON.parse(JSON.stringify(this.grid))
+    const spots = [];
+    let valid = this.isInBounds(word, startPos, dir);
+
+    for(let i = 0; i < word.length && valid; i++) {
+      if(!dupGrid[startPos[0] + i][startPos[1]][0]
+      || dupGrid[startPos[0] + i][startPos[1]][0] === word[i]) {
+        dupGrid[startPos[0] + i][startPos[1]][0] = word[i];
+        spots.push([startPos[0] + i],startPos[1]);
+      } else {
+        valid = false;
+      }
+    }
+
+    if(!valid) {
+      return false;
+    }
+
+    this.isNextVert = false;
+
+    this.grid = dupGrid;
+    this.posArr = this.posArr.concat(spots);
+    this.wordPool.splice(this.wordPool.indexOf(word), 1);
+    this.currentWords.push(word);
+
+    return true;
+  }
+
+  continue() {
+    let valid = false;
+
+    while(!valid) {
+      const data = this.nextWord();
+
+      if(this.isNextVert) {
+        valid = this.vert(data[0], data[1], 0);
+      } else {
+        valid = this.horz(data[0], data[1], 0);
+      }
+    }
+  }
+
+  nextWord() {
+    let valid = false;
+    let pos;
+    let word;
+
+    while(!valid) {
+      word = this.wordPool[Math.floor(Math.random() * this.wordPool.length)];
+
+      for(let i = 0; i < this.posArr.length && !valid; i++) {
+        const letter = this.grid[this.posArr[i][0]][this.posArr[i][1]][0]
+        pos = this.posArr[i];
+
+        valid = letter === word[0];
+        // if(letter === word[0]) {
+        //   const rand = Math.floor(Math.random() * 2);
+
+        //   if(this.isNextVert) {
+        //     valid = this.vert(word, pos, rand);
+        //   } else {
+        //     valid = this.horz(this.wordPool[0], pos, 2 + rand);
+        //   }
+        // }
+      }
+    }
+
+    return [word, pos];
+  }
+
+  // 0,1,2,3 for down, up, right, or left (respectively).
   isInBounds(word, startPos, dir) {
     const start = (dir < 2) ? startPos[0] : startPos[1];
     const end = (dir % 2 === 0) ? start + word.length : start - word.length;
@@ -54,15 +154,15 @@ class Board {
     return randomKey;
   }
 
-  shufflePool(pool) {
-    for(let i = 0; i < pool.length; i++) {
+  shuffleArr(arr) {
+    for(let i = 0; i < arr.length; i++) {
       const j = Math.floor(Math.random() * (i + 1));
-      const temp = pool[i];
-      pool[i] = pool[j];
-      pool[j] = temp;
+      const temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
     }
 
-    return pool;
+    return arr;
   }
 }
 
--- libs/main.js
+++ libs/main.js
@@ -1,6 +1,3 @@
-// const $ = require('jquery');
-// window.jQuery = $;
-// window.$ = $;
 const Board = require("./board").default;
 
 
@@ -10,11 +7,14 @@ $(document).ready(() => {
   for(let y = 0; y < 10; y++) {
     const gridRow = $(`<div class="grid-row" id="${y}"></div>`);
     for(let x = 0; x < 10; x++) {
-      const className = `square${board.grid[y][x] ? " letter" : " no-letter"}`;
-      const square = $(`<div class="${className}" id="${x}">${board.grid[y][x] || " "}</div>`);
+      const className = `square${board.grid[y][x][0] ? " letter" : " no-letter"}`;
+      const square = $(`<div class="${className}" id="${x}">${board.grid[y][x][0] || " "}</div>`);
       square.appendTo(gridRow);
     }
 
     gridRow.appendTo(".board");
   }
+
+  console.log(board.grid);
+  console.log(board.wordPool);
 });
