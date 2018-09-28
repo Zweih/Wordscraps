8385456c28ef70c6a86e04750f6c16db975096a2
Zweih <fernandon299@gmail.com>
2018-09-28 15:13:59 -0400

    Add validation for words. Add board to body.

--- libs/board.js
+++ libs/board.js
@@ -5,13 +5,47 @@ class Board {
     this.grid = new Array(10);
     this.letterPool = this.randLetterPool(wordPools);
     this.wordPool = wordPools[this.letterPool];
-    
-    this.letterPool = this.shufflePool(this.letterPool.split());
+
+    this.letterPool = this.shufflePool(this.letterPool.split()).toString();
     this.wordPool = this.shufflePool(this.wordPool);
 
     for(let i = 0; i < this.grid.length; i++) {
       this.grid[i] = new Array(10);
     }
+
+    console.log(this.letterPool);
+    console.log(this.wordPool);
+
+    this.grid = this.across("yess", [5,5], 0)
+    console.log(this.grid);
+  }
+
+  // 0, 1 for right or left
+  across(word, startPos, dir) {
+    const dupGrid = JSON.parse(JSON.stringify(this.grid))
+    let valid = this.isInBounds(word, startPos, 2 + dir);
+
+    for(let i = 0; i < word.length && valid; i++) {
+      if(!dupGrid[startPos[0]][startPos[1] + i]) {
+        dupGrid[startPos[0]][startPos[1] + i] = word[i];
+      } else {
+        return this.grid;
+      }
+    }
+
+    return dupGrid;
+  }
+
+  // 0,1,2,3 for up, down, right, or left (respectively).
+  isInBounds(word, startPos, dir) {
+    const start = (dir < 2) ? startPos[0] : startPos[1];
+    const end = (dir % 2 === 0) ? start + word.length : start - word.length;
+
+    return end < 10 && end > -1;
+  }
+
+  createWord(string, dir) {
+    return {word: string, dir: dir, links: 0}
   }
 
   randLetterPool(pools) {
@@ -32,6 +66,4 @@ class Board {
   }
 }
 
-const board = new Board;
-console.log(board.letterPool);
-console.log(board.wordPool);
+export default Board;
--- libs/main.js
+++ libs/main.js
@@ -1,1 +1,20 @@
-const Board = require("./board");
+// const $ = require('jquery');
+// window.jQuery = $;
+// window.$ = $;
+const Board = require("./board").default;
+
+
+$(document).ready(() => {
+  const board = new Board();
+
+  for(let y = 0; y < 10; y++) {
+    const gridRow = $(`<div class="grid-row" id="${y}"></div>`);
+    for(let x = 0; x < 10; x++) {
+      const className = `square${board.grid[y][x] ? " letter" : " no-letter"}`;
+      const square = $(`<div class="${className}" id="${x}">${board.grid[y][x] || ""}</div>`);
+      square.appendTo(gridRow);
+    }
+
+    gridRow.appendTo(".board");
+  }
+});
