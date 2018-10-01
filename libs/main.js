91be5f083a13ba46c300a701ee0d20b808382a70
Zweih <fernandon299@gmail.com>
2018-09-30 23:05:14 -0400

    Restructure.

--- libs/main.js
+++ libs/main.js
@@ -1,20 +1,40 @@
-const Board = require("./board").default;
+const board = require("./pine1.json");
 
+let word = "";
+const pool = {};
 
 $(document).ready(() => {
-  const board = new Board();
-
-  for(let y = 0; y < 10; y++) {
+  for(let y = 0; y < board.y; y++) {
     const gridRow = $(`<div class="grid-row" id="${y}"></div>`);
-    for(let x = 0; x < 10; x++) {
-      const className = `square${board.grid[y][x][0] ? " letter" : " no-letter"}`;
-      const square = $(`<div class="${className}" id="${x}">${board.grid[y][x][0] || " "}</div>`);
+    for(let x = 0; x < board.x; x++) {
+      const className = `square${board[y][x] ? " letter" : " no-letter"}`;
+      const square = $(`<div class="${className}" id="${x}"></div>`);
       square.appendTo(gridRow);
     }
 
     gridRow.appendTo(".board");
   }
 
-  console.log(board.grid);
-  console.log(board.wordPool);
+  for(let i = 0; i < board.letters.length; i++) {  
+    const letter = $(
+      `<div
+        class="pool-letter"
+        id="${i}"
+      >
+        ${board.letters[i]}
+      </div>`
+    );
+
+    letter.appendTo(`#${i % 3}.pool-row`).click(letterClick);
+  }
 });
+
+const letterClick = (event) => {
+  word += event.target.outerText;
+
+  if(!pool[event.target.outerText]) {
+    pool[event.target.outerText] = true;
+    console.log(pool);
+  }
+}
+
